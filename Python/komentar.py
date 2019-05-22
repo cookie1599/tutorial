1f62833a45feb8c35cc9bba8e7093c39924989f1
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-05-23 01:16:19 +0700

    Add files via upload

---
+++ Python/komentar.py
@@ -0,0 +1,24 @@
+#ini komentar
+
+#komentar
+#ini
+#hanya
+#bisa
+#digunakan
+#satu
+#baris
+
+print("Hello World") #ini juga komentar
+
+"""
+Ini juga komentar. komentar ini
+lebih dari satu baris hanya 
+menggunakan tanda kutip dua 3 kali dan
+ditutup dengan kutip dua 3 kali juga
+"""
+
+#mencetak nama
+print("Budi")
+
+#mencetak angka
+print(123)
